07c07184cd686152baf6dba6e1319591c9a3839d
ol <olllkarpushkina@gmail.com>
2019-07-19 23:25:13 +0300

    model usage added

--- recognition_with_tesseract.py
+++ recognition_with_tesseract.py
@@ -6,6 +6,8 @@ import os
 image = cv2.imread('example_01.png')
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY)[1]
+cv2.imshow('image', gray)
+
 # write the grayscale image to disk as a temporary file so we can
 # apply OCR to it
 filename = "{}.png".format(os.getpid())
